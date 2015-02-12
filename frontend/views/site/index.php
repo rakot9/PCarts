adba27f1c23016edb306fb871e712ed443d577be
rakot9 <rakot9@yandex.ru>
2015-02-12 11:12:02 +0300

    Main page

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,6 +1,6 @@
 <?php
 /* @var $this yii\web\View */
-$this->title = 'My Yii Application';
+$this->title = 'Ресурсы';
 
 $items = [
     [
@@ -43,12 +43,6 @@ $items = [
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-
         <?= dosamigos\gallery\Carousel::widget([
             'items' => $items, 'json' => true,
             'clientEvents' => [
