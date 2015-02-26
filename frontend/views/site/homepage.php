8e8963f06c8b1544808bedec2fec4b93cf115a0c
rakot9 <rakot9@yandex.ru>
2015-02-26 15:27:43 +0300

    Homepage

--- frontend/views/site/homepage.php
+++ frontend/views/site/homepage.php
@@ -1,2 +1,19 @@
-Домашняя страница
 <?php
+echo \yii\widgets\Menu::widget([
+    'items' => [
+        // Important: you need to specify url as 'controller/action',
+        // not just as 'controller' even if default action is used.
+        ['label' => 'На домашнюю', 'url' => ['site/homepage']],
+        // 'Products' menu item will be selected as long as the route is 'product/index'
+        ['label' => 'Товары', 'url' => ['product/index'], 'items' => [
+            ['label' => 'Загрузить товары', 'url' => ['product/index', 'tag' => 'new']],
+        ]],
+        ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
+    ],
+]);
+
+?>
+
+<div>
+    Content
+</div>
