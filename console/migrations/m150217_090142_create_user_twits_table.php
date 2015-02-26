a26a1b112fe576f1a3af04114af0c19ff1260629
rakot9 <rakot9@yandex.ru>
2015-02-26 16:14:19 +0300

    1

--- console/migrations/m150217_090142_create_user_twits_table.php
+++ console/migrations/m150217_090142_create_user_twits_table.php
@@ -20,7 +20,6 @@ class m150217_090142_create_user_twits_table extends Migration
 
 		$tableOptions = null;
         if ($this->db->driverName === 'mysql') {
-            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
 
