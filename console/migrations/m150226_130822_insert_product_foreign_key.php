93a10b861b0e3e1f347868feb59dcafe64bcd74b
rakot9 <rakot9@yandex.ru>
2015-02-27 10:19:56 +0300

    1

--- console/migrations/m150226_130822_insert_product_foreign_key.php
+++ console/migrations/m150226_130822_insert_product_foreign_key.php
@@ -3,7 +3,7 @@
 use yii\db\Schema;
 use yii\db\Migration;
 
-class m150226_123316_create_description_table extends Migration
+class m150226_130822_insert_product_foreign_key extends Migration
 {
     public $tablePrefix;
     public $tableName;
@@ -11,7 +11,7 @@ class m150226_123316_create_description_table extends Migration
     public function before()
     {
         $this->tablePrefix = Yii::$app->getDb()->tablePrefix;
-        $this->tableName = $this->tablePrefix. 'product_description';
+        $this->tableName = $this->tablePrefix. 'product';
     }
 
     public function up()
@@ -23,15 +23,13 @@ class m150226_123316_create_description_table extends Migration
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
 
-        $this->createTable($this->tableName, [
-            'description_id' => Schema::TYPE_PK . ' UNSIGNED',
-            'description' => Schema::TYPE_TEXT . '(255) NOT NULL'
-        ], $tableOptions);
+        $this->addForeignKey('fk_description', $this->tableName, 'product_description_id', $this->tablePrefix.'product_description', 'description_id');//, 'CASCADE', 'CASCADE'
     }
 
     public function down()
     {
         $this->before();
+        //$this->dropForeignKey('fk_description', $this->tableName);
         $this->dropTable($this->tableName);
     }
 }
