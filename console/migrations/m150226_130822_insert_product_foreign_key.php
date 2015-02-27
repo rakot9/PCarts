c03a7e20ff9cc2952ecdd41b82fb48b1bfafe697
rakot9 <rakot9@yandex.ru>
2015-02-27 10:25:27 +0300

    fr_p_description

--- console/migrations/m150226_130822_insert_product_foreign_key.php
+++ console/migrations/m150226_130822_insert_product_foreign_key.php
@@ -19,17 +19,17 @@ class m150226_130822_insert_product_foreign_key extends Migration
         $this->before();
 
         $tableOptions = null;
+
         if ($this->db->driverName === 'mysql') {
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
 
-        $this->addForeignKey('fk_description', $this->tableName, 'product_description_id', $this->tablePrefix.'product_description', 'description_id', 'CASCADE', 'CASCADE')
+        $this->addForeignKey('fk_description', $this->tableName, 'product_description_id', $this->tablePrefix.'product_description', 'description_id', 'CASCADE', 'CASCADE');
     }
 
     public function down()
     {
         $this->before();
         $this->dropForeignKey('fk_description', $this->tableName);
-        $this->dropTable($this->tableName);
     }
 }
